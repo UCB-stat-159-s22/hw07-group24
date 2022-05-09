e01fb09573553ecb30f904fe1f2abc5a58a6b3fb
George McIntire <george_mcintire@berkeley.edu>
2022-05-09 06:06:12 +0000

    Adding an utils file and updating the EDA notebook

---
+++ notebooks/utils.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+def histboxplot(data, feature, figsize=(12, 7), kde=False, bins=None):
+    """
+    Boxplot and histogram combined
+
+    data: Pandas dataframe
+    feature: Name of the dataframe for visualizing
+    figsize: size of figure (default (12,7))
+    kde: whether to show the density curve (default False)
+    bins: number of bins for histogram (default None)
+    """
+    f2, (ax_box2, ax_hist2) = plt.subplots(
+        nrows=2,  # Number of rows of the subplot grid= 2
+        sharex=True,  # x-axis will be shared among all subplots
+        gridspec_kw={"height_ratios": (0.25, 0.75)},
+        figsize=figsize,
+    )  # creating the 2 subplots
+    sns.boxplot(
+        data=data, x=feature, ax=ax_box2, showmeans=True, color="violet"
+    )  # boxplot will be created and a star will indicate the mean value of the column
+    sns.histplot(
+        data=data, x=feature, kde=kde, ax=ax_hist2, bins=bins, palette="winter"
+    ) if bins else sns.histplot(
+        data=data, x=feature, kde=kde, ax=ax_hist2
+    )  # For histogram
+    ax_hist2.axvline(
+        data[feature].mean(), color="green", linestyle="--"
+    )  # Add mean to the histogram
+    ax_hist2.axvline(
+        data[feature].median(), color="black", linestyle="-"
+    )  # Add median to the histogram
+
+    
+# def model_eval(y_test, y_pred, y_probs):
+# This will be a utils function that outputs a dataframe of a models performance
+# It'll show the accuracy, precision, recall, f1, and auc scores
+
+
+    
